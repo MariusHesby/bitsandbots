97e92a9a83cae76a557c5c92453b664dcb29578d
Marius Rostøl Hesby <marius.r.hesby@gmail.com>
2022-10-07 23:00:35 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const redirects = async () => {
+  return [
+    {
+      source: "/",
+      destination: "/login",
+      permanent: true,
+    },
+  ];
+};
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-    images: {
-    domains: [
-      "media.rawg.io",
-    ],
+  images: {
+    domains: ["media.rawg.io"],
   },
-}
+  redirects: redirects,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
