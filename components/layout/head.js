e2a745eb992a81956a8f975eddf2f165f733cb91
Marius Rostøl Hesby <marius.r.hesby@gmail.com>
2022-10-09 18:27:16 +0200

    Update head.js

--- components/layout/head.js
+++ components/layout/head.js
@@ -10,12 +10,6 @@ export default function Head({ title = "" }) {
       </title>
       <meta name="description" content="Bits & Bots" />
       <link rel="icon" href="/favicon.ico" />
-      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Rock+3D&display=swap"
-        rel="stylesheet"
-      /> */}
     </NextHead>
   );
 }
