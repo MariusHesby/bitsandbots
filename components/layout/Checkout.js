65da97c71de46fee1d81d847c4d579a19275475e
Marius Rostøl Hesby <marius.r.hesby@gmail.com>
2022-10-13 21:23:36 +0200

    Updates

--- components/layout/Checkout.js
+++ components/layout/Checkout.js
@@ -14,7 +14,7 @@ function Checkout({ setShowModal }) {
         <div className="max-w-6xl bg-white w-full rounded-lg py-10 px-5 drop-shadow-lg">
           <h2 className="mb-10">Checkout</h2>
           <div className="mb-10">
-            <p className="text-2xl">
+            <p className="text-base sm:text-2xl">
               You have
               <span className="border-2 border-blue-600 rounded-lg px-2 py-1/2 mx-3 font-bold">
                 {cart.length}
@@ -22,7 +22,7 @@ function Checkout({ setShowModal }) {
               {cart.length === 1 ? "item" : "items"}
             </p>
 
-            <p className="text-2xl">in your shopping-cart</p>
+            <p className="text-base sm:text-2xl">in your shopping-cart</p>
           </div>
           <ContactForm setShowModal={setShowModal} />
         </div>
--- components/layout/TinyHero.js
+++ components/layout/TinyHero.js
@@ -1,10 +1,10 @@
 function TinyHero() {
   return (
-    <div className="mx-5 my-10 bg-blue-600 rounded-3xl py-10 px-5 text-center">
+    <div className="mx-5 my-10 bg-blue-600 rounded-3xl py-6 sm:py-10 px-5 text-center">
       <h1 className="text-center font-nabla mb-5">
         Bits & Bots - games for every platform!
       </h1>
-      <p className="text-xl sm:text-3xl text-white">
+      <p className="text-xl sm:text-3xl text-white hidden sm:block">
         Every games has it's day!
       </p>
     </div>
